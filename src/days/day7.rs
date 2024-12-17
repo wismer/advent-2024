098bad1761fabc273d51d00c4c0eee75eb3a6eab
Matt Long <matthewhl@gmail.com>
2024-12-16 21:58:57 -0500

    p2

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -6,10 +6,32 @@ struct Equation {
     numbers: Vec<usize>
 }
 
-enum Op {
-    Mult,
-    Add
+// I did not write this, adam chalmers did. Thanks/Sorry adam! I don't understand math!
+fn is_solvable(goal: usize, items: Vec<usize>, allow_concat: bool) -> bool {
+    // Base case.
+    let Some((curr, rest)) = items.split_last() else {
+        return goal == 0;
+    };
+
+    // Three recursive cases:
+    // 1. Using + operation
+    let diff = if goal > *curr {
+        goal - curr
+    } else {
+        curr - goal
+    };
+    println!("curr {}, goal {}", curr, goal);
+    is_solvable(diff, rest.to_vec(), allow_concat)
+    // 2. Using * operation
+    || goal % curr == 0 && is_solvable(goal / curr, rest.to_vec(), allow_concat)
+    // 3. Using || operation
+    || {
+        let new_goal = diff;
+        let tens = 10usize.pow(curr.ilog10() + 1);
+        allow_concat && new_goal % tens == 0 && is_solvable(new_goal / tens, rest.to_vec(), allow_concat)
+    }
 }
+
 
 impl Equation {
     fn operations(&self, val: usize, idx: usize) -> bool {
@@ -18,9 +40,23 @@ impl Equation {
                 self.operations(val + n, idx + 1) || self.operations(val * n, idx + 1)
             },
             None => {
-                val == self.result
+                if val == self.result {
+                    true
+                } else {
+                    self.operations_with_concatenate()
+                }
             }
         }
+    }
+
+    fn operations_with_concatenate(&self, ) -> bool {
+        false
+    }
+
+    fn concatenate(&self, x: usize, y: usize) -> usize {
+        let mut r = x.to_string();
+        r.push_str(y.to_string().as_str());
+        r.parse::<usize>().unwrap()
     }
 }
 
@@ -62,7 +98,15 @@ impl Day7 for AdventSolver {
     }
 
     fn part_two(&self) -> usize {
-        0
+        let data = self.parse();
+        let mut result = 0;
+        for equation in data {
+            if is_solvable(equation.result, equation.numbers, true) {
+                result += equation.result;
+            }
+        }
+
+        result
     }
 }
 
@@ -85,4 +129,16 @@ fn test_part_one() {
     let result = solver.part_one();
 
     assert_eq!(result, 3749);
+}
+
+#[test]
+fn test_concatenate() {
+    let eq = Equation {
+        numbers: vec![],
+        result: 0
+    };
+
+    let result = eq.concatenate(12, 125);
+
+    assert_eq!(result, 12125);
 }
